03d61c7539e5e7528bb7212b7684f879ba0afaff
Daniel <danialexander.dev@gmail.com>
2025-08-11 19:06:03 -0500

    Herramienta para crear contraseñas en python version mejorada

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hola Mundo")
+import string
+import random
+
+longitud= int(input("Ingrese el tamaño de la contraseña"))
+caracteres=string.ascii_letters + string.digits + string.punctuation
+contrasena= "".join(ranmdom.choice(caracteres) for i in range(longitud))
+print("La contraseña generada es : "+contrasena)
